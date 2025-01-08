12d8ac1abad278ff4af31e65f83df150310dfe24
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-08 23:47:59 +0530

    Updated array day 2.cpp with recent changes

--- array day 2.cpp
+++ array day 2.cpp
@@ -61,3 +61,32 @@ int main() {
    int result = replaceZerosWithOnes(n);
    cout << "After replacing zeros with ones " << n << " becomes " << result;
 }
+
+
+
+// calculate the area of circle---------->
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Area {
+
+  public:
+
+    void areaOfCircle(int n) {
+
+      double ans = 3.14 * n * n; // Area of circle = πr2
+      cout << "Area of circle is : ";
+      cout << ans;
+    }
+};
+int main() {
+
+  int n = 5;
+
+  Area p1;
+  p1.areaOfCircle(n);
+
+  return 0;
+}
