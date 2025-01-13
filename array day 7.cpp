dfe16d9e23ea530276a430ef8743fd8da33817f2
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-13 23:24:17 +0530

    updated array day 7

---
+++ array day 7.cpp
@@ -0,0 +1,17 @@
+//letcode -- 3223 Minimum Length of String After Operations
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector <int>freq(26);
+        for (int i=0;s[i];++i)
+            freq[s[i]-'a']++;
+        
+        int length=0;
+        for (int i=0;i<26;i++){
+            if(freq[i]&1)   length+=1;
+            else            length+=min(2,freq[i]);
+        }
+        return length;
+    }
+};
