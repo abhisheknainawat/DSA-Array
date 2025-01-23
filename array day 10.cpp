32659e3d9590004e90d01ed6217b090cb173d531
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-23 22:55:10 +0530

    updated array day 10

---
+++ array day 10.cpp
@@ -0,0 +1,27 @@
+// leetcode 121---->
+
+class Solution {
+public:
+    void  maxProfitFinder(vector<int>& prices,int i, int&minPrice,int&maxProfit){
+
+        //base case
+        if(i==prices.size()) return;
+
+        //ek case handle
+    
+        if(prices[i]<minPrice) minPrice=prices[i];
+        int todaysProfit=prices[i]-minPrice;
+        if(todaysProfit>maxProfit) maxProfit=todaysProfit;
+
+        //RE
+        maxProfitFinder(prices,i+1,minPrice,maxProfit);
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int minPrice=INT_MAX;
+        int maxProfit=INT_MIN;
+        maxProfitFinder(prices,0,minPrice,maxProfit);
+        return maxProfit;
+        
+    }
+};
