7646b6e51be9570677492de51dc07b3315f5d6ec
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-29 18:49:45 +0530

    basic dsa

--- 30 day dsa.cpp
+++ 30 day dsa.cpp
@@ -134,3 +134,37 @@ public:
         
     }
 };
+
+//leaders in an array
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+void findleaders(vector<int>&arr,int n){
+    // vector<int> myArray;
+    arr.push_back(arr[n]-1);
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[i+1]){
+            if(arr[i]==arr[n]){
+                arr[i+1]==-1;
+            }
+            cout<<arr[i]<<" ";
+            
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    vector<int>arr={5,7,2,0};
+    int n=4;
+    findleaders(arr,n);
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<","<<endl;
+    // }
+    
+    return 0;
+}
