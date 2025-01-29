06243c3ccec008558cfb13878c7ecd3c4d960d0c
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-29 18:17:39 +0530

    basic dsa

--- 30 day dsa.cpp
+++ 30 day dsa.cpp
@@ -113,3 +113,24 @@ int main() {
 	}
 	cout <<"The sum of the elements of the array is "<< sum;
 }
+
+//leetcode 74 search in a 2d array
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0 , high=n*m-1;
+        while(low<= high){
+            int mid=(low+high)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
+
+        }
+        return false;
+        
+    }
+};
