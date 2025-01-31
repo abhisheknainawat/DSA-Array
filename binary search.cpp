810721e9d3c1225d41f4a584f8762a83f8e048a1
Abhishek Nainawat <141261478+abhisheknainawat@users.noreply.github.com>
2025-01-31 16:29:34 +0530

    basic binary search

--- binary search.cpp
+++ binary search.cpp
@@ -14,3 +14,21 @@ public:
         
     }
 };
+
+//leetcode 35 search insertion position
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0,e=nums.size()-1;
+        while(s<=e){
+            int m=(s+e)/2;
+            if(nums[m]==target) return m;
+            else if(nums[m]<target) s=m+1;
+            else e=m-1;
+
+        }
+        return -1;
+        
+    }
+};
